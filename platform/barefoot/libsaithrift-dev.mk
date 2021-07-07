d65ec956bfc188de6d0a0249d9480ef80beda036
Your Name <you@example.com>
2021-07-07 12:16:06 +0000

    init

--- platform/barefoot/libsaithrift-dev.mk
+++ platform/barefoot/libsaithrift-dev.mk
@@ -1,20 +1,21 @@
 # libsaithrift-dev package
-
-SAI_VER = 0.9.4
-
-LIBSAITHRIFT_DEV = libsaithrift-dev_$(SAI_VER)_amd64.deb
+SAITHRIFT_VER = 0.9.4
+LIBSAITHRIFT_DEV = libsaithrift-dev_$(SAITHRIFT_VER)_$(CONFIGURED_ARCH).deb
 $(LIBSAITHRIFT_DEV)_SRC_PATH = $(SRC_PATH)/sonic-sairedis/SAI
-$(LIBSAITHRIFT_DEV)_DEPENDS += $(LIBTHRIFT) $(LIBTHRIFT_DEV) $(PYTHON_THRIFT) $(THRIFT_COMPILER) $(BFN_SAI) $(BFN_SAI_DEV)
-$(LIBSAITHRIFT_DEV)_RDEPENDS += $(LIBTHRIFT) $(BFN_SAI)
+$(LIBSAITHRIFT_DEV)_DEPENDS += $(BFN_LIBTHRIFT) $(BFN_LIBTHRIFT_DEV) \
+                               $(BFN_PYTHON_THRIFT) $(BFN_THRIFT_COMPILER)  \
+                               $(BFN_SAI)
+
+$(LIBSAITHRIFT_DEV)_RDEPENDS += $(BFN_LIBTHRIFT) $(BFN_SAI)
 SONIC_DPKG_DEBS += $(LIBSAITHRIFT_DEV)
 
-PYTHON_SAITHRIFT = python-saithrift_$(SAI_VER)_amd64.deb
+PYTHON_SAITHRIFT = python-saithrift_$(SAITHRIFT_VER)_amd64.deb
 $(eval $(call add_extra_package,$(LIBSAITHRIFT_DEV),$(PYTHON_SAITHRIFT)))
 
-SAISERVER = saiserver_$(SAI_VER)_amd64.deb
-$(SAISERVER)_RDEPENDS += $(LIBTHRIFT) $(BRCM_SAI)
+SAISERVER = saiserver_$(SAITHRIFT_VER)_amd64.deb
+$(SAISERVER)_RDEPENDS += $(BFN_LIBTHRIFT) $(BFN_SAI)
 $(eval $(call add_extra_package,$(LIBSAITHRIFT_DEV),$(SAISERVER)))
 
-SAISERVER_DBG = saiserver-dbg_$(SAI_VER)_amd64.deb
+SAISERVER_DBG = saiserver-dbg_$(SAITHRIFT_VER)_amd64.deb
 $(SAISERVER_DBG)_RDEPENDS += $(SAISERVER)
 $(eval $(call add_extra_package,$(LIBSAITHRIFT_DEV),$(SAISERVER_DBG)))
